16f0d346b71ac3703cc38145f07d4ac0fcb969e4
surya-sukumar <79688972+surya-sukumar@users.noreply.github.com>
2024-09-01 00:27:31 -0400

    creating chat page

---
+++ src/components/UserContext.js
@@ -0,0 +1,14 @@
+import React, { createContext, useState } from 'react';
+
+// Create a context with an initial value of null
+export const UserContext = createContext(null);
+
+export const UserProvider = ({ children }) => {
+    const [userId, setUserId] = useState(null);
+
+    return (
+        <UserContext.Provider value={{ userId, setUserId }}>
+            {children}
+        </UserContext.Provider>
+    );
+};
