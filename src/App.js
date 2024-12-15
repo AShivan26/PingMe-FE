2b62e9f8f13bbc8dbddb95e152c0a13eb81a5d5d
Shourya N Kumar <shouryank01@gmail.com>
2024-12-15 13:00:35 -0500

    Revamping the ui/ux for login and register

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import './App.css';
 import LoginForm from "./components/LoginForm";
-import { BrowserRouter as Router, Routes, Route, Navigate, useNavigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import RegisterForm from "./components/RegisterForm";
 import { UserProvider } from './components/UserContext';
 import ChatPage from "./components/ChatPage";
 
 function App() {
-
-
       return (
           <UserProvider>
               <Router>
-                <div className="App">
-                  <header className="Ping-Me-Chat">
-                      <Routes>
-                          <Route path="/login" element={<LoginForm />}  />
-                          <Route path="/register" element={<RegisterForm />} />
-                          <Route path="/chat" element={<ChatPage/>} />
-                          <Route path="*" element={<Navigate to="/login" replace />} />
-                      </Routes>
-                  </header>
-                </div>
+                  <Routes>
+                      <Route path="/login" element={<LoginForm />}  />
+                      <Route path="/register" element={<RegisterForm />} />
+                      <Route path="/chat" element={<ChatPage/>} />
+                      <Route path="*" element={<Navigate to="/login" replace />} />
+                  </Routes>
               </Router>
           </UserProvider>
       );
